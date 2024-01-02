94e7617971173de5a3923c9f0993831dc1087d56
Grandhi Siddu <113771456+sidd9390@users.noreply.github.com>
2024-01-03 01:18:19 +0530

    modified:   Contact_us.html 	modified:   Grade_1.html 	modified:   Grade_2.html 	modified:   Grade_3.html 	modified:   Grade_4.html 	modified:   Grade_5.html 	modified:   Home.html 	new file:   Img/14.png 	new file:   Img/2.png 	new file:   Img/3.png 	new file:   Img/4.png 	new file:   Img/5.png 	new file:   Img/7.png 	new file:   Img/8.png 	new file:   Img/Nursery CB 1.png 	modified:   KG_ENG_CL.html 	modified:   LKG.html 	modified:   LKG_ENG_CL.html 	modified:   LKG_GEN_CL.html 	modified:   LKG_MATH_CL.html 	modified:   Profile.html 	modified:   UKG.html 	modified:   index.html 	modified:   nursery.html 	new file:   script.js 	modified:   style.css

---
+++ script.js
@@ -0,0 +1,15 @@
+function KG1(){
+    document.getElementById("KG_EN").style.display = "block";
+    document.getElementById("KG_MA").style.display = "none";
+    document.getElementById("KG_GA").style.display = "none";
+}
+function KG2(){
+    document.getElementById("KG_EN").style.display = "none";
+    document.getElementById("KG_MA").style.display = "block";
+    document.getElementById("KG_GA").style.display = "none";
+}
+function KG3(){
+    document.getElementById("KG_EN").style.display = "none";
+    document.getElementById("KG_MA").style.display = "none";
+    document.getElementById("KG_GA").style.display = "block";
+}
