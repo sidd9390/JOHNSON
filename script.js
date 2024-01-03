a252bed2ae02a7ba412257b14b9dcbb82767c0c5
Grandhi Siddu <113771456+sidd9390@users.noreply.github.com>
2024-01-03 19:56:36 +0530

    new file:   .vscode/settings.json 	new file:   LKG CB 1.jpg 	modified:   LKG.html 	new file:   UKG CB 1.jpg 	modified:   UKG.html 	modified:   nursery.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+function LKG1(){
+    document.getElementById("LKG_EN").style.display = "block";
+    document.getElementById("LKG_MA").style.display = "none";
+    document.getElementById("LKG_GA").style.display = "none";
+}
+function LKG2(){
+    document.getElementById("LKG_EN").style.display = "none";
+    document.getElementById("LKG_MA").style.display = "block";
+    document.getElementById("LKG_GA").style.display = "none";
+}
+function LKG3(){
+    document.getElementById("LKG_EN").style.display = "none";
+    document.getElementById("LKG_MA").style.display = "none";
+    document.getElementById("LKG_GA").style.display = "block";
+}
+function UKG1(){
+    document.getElementById("UKG_EN").style.display = "block";
+    document.getElementById("UKG_MA").style.display = "none";
+    document.getElementById("UKG_GA").style.display = "none";
+}
+function UKG2(){
+    document.getElementById("UKG_EN").style.display = "none";
+    document.getElementById("UKG_MA").style.display = "block";
+    document.getElementById("UKG_GA").style.display = "none";
+}
+function UKG3(){
+    document.getElementById("UKG_EN").style.display = "none";
+    document.getElementById("UKG_MA").style.display = "none";
+    document.getElementById("UKG_GA").style.display = "block";
+}
 function KG1(){
     document.getElementById("KG_EN").style.display = "block";
     document.getElementById("KG_MA").style.display = "none";
