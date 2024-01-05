e7a857f707f03d97cd59dde9864ec3b17a3cb1cd
Grandhi Siddu <113771456+sidd9390@users.noreply.github.com>
2024-01-05 15:51:25 +0530

    new file:   assets/Images/EVS Sem G1 V1.jpg 	new file:   assets/Images/EVS Sem G1 V2-1.jpg 	new file:   assets/Images/English Sem G1 V1.jpg 	new file:   assets/Images/Grade-2/EVS Class 2 Vol 1.jpg 	new file:   assets/Images/Grade-2/EVS Sem G2 V1.jpg 	new file:   assets/Images/Grade-2/English Class 2 Vol 2.jpg 	new file:   assets/Images/Grade-2/English Sem G2 V1.jpg 	new file:   assets/Images/Grade-2/Maths Class 2 Vol 1.jpg 	new file:   assets/Images/Grade-2/Maths Sem G2 V1.jpg 	new file:   assets/Images/Grade-3/EVS Class 3 Vol 2.jpg 	new file:   assets/Images/Grade-3/EVS Sem G3 V1.jpg 	new file:   assets/Images/Grade-3/English Class 3 Vol 1.jpg 	new file:   assets/Images/Grade-3/English Sem G3 V1.jpg 	new file:   assets/Images/Grade-3/MATHS Class 3 Vol 1.jpg 	new file:   assets/Images/Grade-3/Maths Sem G3 V1.jpg 	new file:   assets/Images/Grade-4/EVS Class 4 Vol 1.jpg 	new file:   assets/Images/Grade-4/EVS Sem G4 V1.jpg 	new file:   assets/Images/Grade-4/English Class 4 Vol 1.jpg 	new file:   assets/Images/Grade-4/English Sem G4 V1.jpg 	new file:   assets/Images/Grade-4/MATHS Class 4 Vol 1.jpg 	new file:   assets/Images/Grade-4/MATHS Sem G4 V1.jpg 	new file:   assets/Images/Grade-5/ENGLISH Class 5 Vol 1.jpg 	new file:   assets/Images/Grade-5/ENGLISH Sem G5 V1.jpg 	new file:   assets/Images/Grade-5/EVS Class 5 Vol 1.jpg 	new file:   assets/Images/Grade-5/EVS Sem G5 V1.jpg 	new file:   assets/Images/Grade-5/MATHS Class 5 Vol 1.jpg 	new file:   assets/Images/Grade-5/MATHS Sem G5 V1.jpg 	modified:   assets/Navbar_HTML/Grade_1.html 	modified:   assets/Navbar_HTML/Grade_2.html 	modified:   assets/Navbar_HTML/Grade_3.html 	modified:   assets/Navbar_HTML/Grade_4.html 	modified:   assets/Navbar_HTML/Grade_5.html 	modified:   assets/Navbar_HTML/Home.html 	modified:   assets/css/style.css 	modified:   assets/js/script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,6 +43,21 @@ function G_1_3(){
     document.getElementById("G_1_MA").style.display = "none";
     document.getElementById("G_1_GA").style.display = "block";
 }
+function G_2_1(){
+    document.getElementById("G_2_EN").style.display = "block";
+    document.getElementById("G_2_MA").style.display = "none";
+    document.getElementById("G_2_GA").style.display = "none";
+}
+function G_2_2(){
+    document.getElementById("G_2_EN").style.display = "none";
+    document.getElementById("G_2_MA").style.display = "block";
+    document.getElementById("G_2_GA").style.display = "none";
+}
+function G_2_3(){
+    document.getElementById("G_2_EN").style.display = "none";
+    document.getElementById("G_2_MA").style.display = "none";
+    document.getElementById("G_2_GA").style.display = "block";
+}
 function KG1(){
     document.getElementById("KG_EN").style.display = "block";
     document.getElementById("KG_MA").style.display = "none";
@@ -57,4 +72,113 @@ function KG3(){
     document.getElementById("KG_EN").style.display = "none";
     document.getElementById("KG_MA").style.display = "none";
     document.getElementById("KG_GA").style.display = "block";
+}
+function G_3_1(){
+    document.getElementById("G_3_EN").style.display = "block";
+    document.getElementById("G_3_MA").style.display = "none";
+    document.getElementById("G_3_GA").style.display = "none";
+    document.getElementById("G_3_SI").style.display = "none";
+    document.getElementById("G_3_SO").style.display = "none";
+}
+function G_3_2(){
+    document.getElementById("G_3_EN").style.display = "none";
+    document.getElementById("G_3_MA").style.display = "block";
+    document.getElementById("G_3_GA").style.display = "none";
+    document.getElementById("G_3_SI").style.display = "none";
+    document.getElementById("G_3_SO").style.display = "none";
+}
+function G_3_3(){
+    document.getElementById("G_3_EN").style.display = "none";
+    document.getElementById("G_3_MA").style.display = "none";
+    document.getElementById("G_3_GA").style.display = "block";
+    document.getElementById("G_3_SI").style.display = "none";
+    document.getElementById("G_3_SO").style.display = "none";
+}
+function G_3_4(){
+    document.getElementById("G_3_EN").style.display = "none";
+    document.getElementById("G_3_MA").style.display = "none";
+    document.getElementById("G_3_GA").style.display = "none";
+    document.getElementById("G_3_SI").style.display = "block";
+    document.getElementById("G_3_SO").style.display = "none";
+}
+function G_3_5(){
+    document.getElementById("G_3_EN").style.display = "none";
+    document.getElementById("G_3_MA").style.display = "none";
+    document.getElementById("G_3_GA").style.display = "none";
+    document.getElementById("G_3_SI").style.display = "none";
+    document.getElementById("G_3_SO").style.display = "block";
+}
+function G_4_1(){
+    document.getElementById("G_4_EN").style.display = "block";
+    document.getElementById("G_4_MA").style.display = "none";
+    document.getElementById("G_4_GA").style.display = "none";
+    document.getElementById("G_4_SI").style.display = "none";
+    document.getElementById("G_4_SO").style.display = "none";
+}
+function G_4_2(){
+    document.getElementById("G_4_EN").style.display = "none";
+    document.getElementById("G_4_MA").style.display = "block";
+    document.getElementById("G_4_GA").style.display = "none";
+    document.getElementById("G_4_SI").style.display = "none";
+    document.getElementById("G_4_SO").style.display = "none";
+}
+function G_4_3(){
+    document.getElementById("G_4_EN").style.display = "none";
+    document.getElementById("G_4_MA").style.display = "none";
+    document.getElementById("G_4_GA").style.display = "block";
+    document.getElementById("G_4_SI").style.display = "none";
+    document.getElementById("G_4_SO").style.display = "none";
+}
+function G_4_4(){
+    document.getElementById("G_4_EN").style.display = "none";
+    document.getElementById("G_4_MA").style.display = "none";
+    document.getElementById("G_4_GA").style.display = "none";
+    document.getElementById("G_4_SI").style.display = "block";
+    document.getElementById("G_4_SO").style.display = "none";
+}
+function G_4_5(){
+    document.getElementById("G_4_EN").style.display = "none";
+    document.getElementById("G_4_MA").style.display = "none";
+    document.getElementById("G_4_GA").style.display = "none";
+    document.getElementById("G_4_SI").style.display = "none";
+    document.getElementById("G_4_SO").style.display = "block";
+}
+
+
+
+
+function G_5_1(){
+    document.getElementById("G_5_EN").style.display = "block";
+    document.getElementById("G_5_MA").style.display = "none";
+    document.getElementById("G_5_GA").style.display = "none";
+    document.getElementById("G_5_SI").style.display = "none";
+    document.getElementById("G_5_SO").style.display = "none";
+}
+function G_5_2(){
+    document.getElementById("G_5_EN").style.display = "none";
+    document.getElementById("G_5_MA").style.display = "block";
+    document.getElementById("G_5_GA").style.display = "none";
+    document.getElementById("G_5_SI").style.display = "none";
+    document.getElementById("G_5_SO").style.display = "none";
+}
+function G_5_3(){
+    document.getElementById("G_5_EN").style.display = "none";
+    document.getElementById("G_5_MA").style.display = "none";
+    document.getElementById("G_5_GA").style.display = "block";
+    document.getElementById("G_5_SI").style.display = "none";
+    document.getElementById("G_5_SO").style.display = "none";
+}
+function G_5_4(){
+    document.getElementById("G_5_EN").style.display = "none";
+    document.getElementById("G_5_MA").style.display = "none";
+    document.getElementById("G_5_GA").style.display = "none";
+    document.getElementById("G_5_SI").style.display = "block";
+    document.getElementById("G_5_SO").style.display = "none";
+}
+function G_5_5(){
+    document.getElementById("G_5_EN").style.display = "none";
+    document.getElementById("G_5_MA").style.display = "none";
+    document.getElementById("G_5_GA").style.display = "none";
+    document.getElementById("G_5_SI").style.display = "none";
+    document.getElementById("G_5_SO").style.display = "block";
 }
